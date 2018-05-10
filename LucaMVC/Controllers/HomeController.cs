96ecdf0d3b5f9360703297867388f2992f9d99a5
LukeGent <lucgent@gmail.com>
2018-05-10 16:21:05 +0200

    add view pranzo e cena

--- LucaMVC/Controllers/HomeController.cs
+++ LucaMVC/Controllers/HomeController.cs
@@ -21,5 +21,38 @@ namespace LucaMVC.Controllers {
 
 			return View();
 		}
+
+		public ActionResult Pranzo() {
+			ViewBag.Message = "Inserisci il pranzo";
+			return View("Pranzo");
+		}
+
+		[HttpPost]
+		public ActionResult Pranzo(DateTime date, string primo, string secondo, string contorno, string dolce) {
+			ViewBag.Message = "Inserisci il pranzo";
+			@ViewBag.p_data = date;
+			ViewBag.p_primo = primo ;
+			ViewBag.p_secondo = secondo;
+			ViewBag.p_contorno = contorno;
+			ViewBag.p_dolce = dolce;
+			return View("Pranzo");
+		}
+
+		public ActionResult Cena() {
+			ViewBag.Message = "Inserisci il pranzo";
+			return View("Cena");
+		}
+
+		[HttpPost]
+		public ActionResult Cena(DateTime date, string primo, string secondo, string contorno, string dolce) {
+			ViewBag.Message = "Inserisci la cena";
+			@ViewBag.c_data = date;
+			ViewBag.c_primo = primo;
+			ViewBag.c_secondo = secondo;
+			ViewBag.c_contorno = contorno;
+			ViewBag.c_dolce = dolce;
+			return View("Cena");
+		}
+
 	}
 }
