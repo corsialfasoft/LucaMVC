3fadada01efe57c5a398cf2d62d1254a2ea24484
LukeGent <lucgent@gmail.com>
2018-05-16 10:20:10 +0200

    inserimento dati di un menu nel database ok

--- LucaMVC/Models/DomainModel.cs
+++ LucaMVC/Models/DomainModel.cs
@@ -8,7 +8,42 @@ using DataTransferObjects;
 namespace LucaMVC.Models {
 	public class DomainModel {
 		public void AddMenu(DTMenu menu) {
-			
+			using(var db = new RistoranteEntities1()){
+				var pri = new Primo {
+					nome = menu.Primo[0].NomePrimo,
+				};
+				db.Primo.Add(pri);
+				db.SaveChanges();
+
+				var sec = new Secondo {
+					nome = menu.Secondo[0].NomeSecondo,
+				};
+				db.Secondo.Add(sec);
+				db.SaveChanges();
+
+				var cont = new Contorno {
+					nome = menu.Contorno[0].NomeContorno,
+				};
+				db.Contorno.Add(cont);
+				db.SaveChanges();
+
+				var dol = new Dolce {
+					nome = menu.Dolce[0].NomeDolce,
+				};
+				db.Dolce.Add(dol);
+				db.SaveChanges();
+
+				var men = new Menu {
+					dataMenu = menu.DataMenu,
+					tipoMenu = $"{menu.Tipologia_Pasto}",
+					primo = pri.id,
+					secondo = sec.id,
+					contorno = cont.id,
+					dolce = dol.id,
+				};
+				db.Menu.Add(men);
+				db.SaveChanges();
+			}
 		}
 
 		public void Giornata(DateTime date) {
